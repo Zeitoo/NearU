d8908eab93444b680b56292a21339a1d76991545
zeito <josejoaozeito@gmail.com>
2026-02-23 18:16:14 +0000

    Stable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,24 @@
-import http from "http";
+import https from "https";
 import createApp from "./app";
 import { onlineUsers } from "./state/onLineUsers";
 import { setupWebSocket } from "./websocket/server";
+import fs from "fs";
 export default function startServer() {
 	const app = createApp();
-	const server = http.createServer(app);
+
+	const config = {
+		key: fs.readFileSync("../localhost+3-key.pem"),
+		cert: fs.readFileSync("../localhost+3.pem"),
+	};
+
+	const server = https.createServer(config, app);
 
 	setupWebSocket(server);
 	server.listen(3000, () => {
-		console.log("Server started at http://localhost:3000");
+		console.log("Server started at https://localhost:3000");
 	});
 
 	server.on("error", (error: NodeJS.ErrnoException) => {
 		console.error("Erro no servidor:", error);
 	});
-
 }
