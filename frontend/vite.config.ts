b5dd5bc0e2267645b81025f7a79205766ac2ae22
zeito <josejoaozeito@gmail.com>
2026-02-24 23:17:11 +0000

    Packages optimized 1.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
 		host: true,
 		port: 2000,
-		https: {
-			key: fs.readFileSync("../localhost+3-key.pem"),
-			cert:fs.readFileSync("../localhost+3.pem") ,
-		}
-
 	},
 });
