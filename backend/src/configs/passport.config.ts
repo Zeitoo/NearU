e6b044f5182e0028c0274dbf410e3d6a550fa9b4
zeito <josejoaozeito@gmail.com>
2026-02-25 00:03:55 +0000

    Passport packages added.

--- backend/src/configs/passport.config.ts
+++ backend/src/configs/passport.config.ts
@@ -1,7 +1,6 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { putUserByGoogle } from "../models/auth.model";
-import { string } from "zod";
 
 passport.use(
 	new GoogleStrategy(
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,10 @@
-import https from "https";
+import http from "http";
 import createApp from "./app";
 import { setupWebSocket } from "./websocket/server";
-import fs from "fs";
 export default function startServer() {
 	const app = createApp();
 
-	const config = {
-		key: fs.readFileSync("../localhost+3-key.pem"),
-		cert: fs.readFileSync("../localhost+3.pem"),
-	};
-
-	const server = https.createServer(config, app);
+	const server = http.createServer(app);
 
 	setupWebSocket(server);
 	server.listen(3000, () => {
@@ -20,5 +14,4 @@ export default function startServer() {
 	server.on("error", (error: NodeJS.ErrnoException) => {
 		console.error("Erro no servidor:", error);
 	});
-
 }
